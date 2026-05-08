ec13ab85221aba5a7a2bbf75a8c5dcc7cac76da6
unknown <Hothri03>
2026-05-09 01:15:01 +0530

    linkedin and address added.

---
+++ test.js
@@ -0,0 +1,4 @@
+// const fs = require('fs');
+// const content = fs.readFileSync('index.html', 'utf8');
+// const index = content.indexOf('data-framer-name="FAQ"');
+// console.log(content.substring(index, index + 600));
